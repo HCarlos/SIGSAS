2e6b8d4634f0dd9b128819e52d99e7181a1d99d6
HCarlos <r0@tecnointel.mx>
2022-04-12 22:48:25 +0000

    sigsas - A-012 | Alfa L8.83.6

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.livewire-app')
 
 @section('content')
+    @livewire('users-table')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
